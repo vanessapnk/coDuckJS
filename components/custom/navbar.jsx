638e4014b96f7c17fe833c700709bfa15c95da49
Leandro Macedo <leandro.smacedo@Live.com>
2024-03-02 21:28:37 +0000

    feat: profile

---
+++ components/custom/navbar.jsx
@@ -0,0 +1,9 @@
+export function Navbar() {
+    return (
+        <>
+            <div className="bg-zinc-800 h-8 w-full text-center">
+                <p>navbar</p>
+            </div>
+        </>
+    )
+}
