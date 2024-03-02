53bd11901ba2eb4e99c3b569d4040a1430e3854c
Vanessa Silva <156242127+vanessapnk@users.noreply.github.com>
2024-03-02 18:13:11 +0000

    login e signup

---
+++ pages/api/login.js
@@ -0,0 +1,10 @@
+import { loginHandler } from "@/src/services/users";
+
+
+export default async function handler(req, res) {
+    if (req.method === 'POST') {
+        await loginHandler(req, res);
+    } else {
+        res.status(405).json({ message: 'Method Not Allowed' });
+    }
+}
