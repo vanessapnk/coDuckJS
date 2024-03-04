c6d6250fab08803c1ce62596325486608346a017
vasco2024 <156312623+vasco2024@users.noreply.github.com>
2024-03-04 00:11:06 +0000

    w

--- pages/groups.jsx
+++ pages/groups.jsx
@@ -5,6 +5,7 @@ import { CardProfile } from "@/components/profile/cardProfile";
 
 export default function Groups() {
   const [groups, setGroups] = useState([]);
+  const [showFilters, setShowFilters] = useState(false); // State to manage filter options visibility
   const router = useRouter();
 
   const fetchData = async () => {
@@ -21,30 +22,47 @@ export default function Groups() {
     fetchData();
   }, []);
 
+  const toggleFilters = () => {
+    setShowFilters(!showFilters); // Toggles the visibility of filter options
+  };
+
   return (
     <>
-      <div className="flex flex-col gap-4 pb-12">
+      <div className="flex justify-between items-center gap-4 pb-12">
         <h1 className="text-2xl font-medium">Explore All Groups</h1>
-        <div>
-          <div className="flex flex-col gap-4">
-            {groups.map((group) => (
-              <CardProfile
-                key={group.id}
-                category="Group"
-                profileCheck={true}
-                location={group.city}
-                profileImage={
-                  group.photo_url ||
-                  "https://images.pexels.com/photos/3471028/pexels-photo-3471028.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                }
-                title={group.name}
-                description={group.description}
-                likes={group.likes}
-                stacks={group.stacks}
-              />
-            ))}
-          </div>
+        <button
+          onClick={toggleFilters}
+          className="px-4 py-2 bg-gray-700 hover:bg-gray-800 text-white rounded-md shadow"
+        >
+          Filter
+        </button>
+      </div>
+
+      {showFilters && (
+        <div className="bg-gray-100 p-4 mb-4 rounded-md shadow">
+          {/* Implement your filter options component here */}
+          {/* Example: */}
+          <p>Filter options go here...</p>
         </div>
+      )}
+
+      <div className="flex flex-col gap-4">
+        {groups.map((group) => (
+          <CardProfile
+            key={group.id}
+            category="Group"
+            profileCheck={true}
+            location={group.location}
+            profileImage={
+              group.photo_url ||
+              "https://images.pexels.com/photos/3471028/pexels-photo-3471028.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            }
+            title={group.name}
+            description={group.description}
+            likes={group.likes}
+            stacks={group.stacks}
+          />
+        ))}
       </div>
 
       <Navbar homeActive={false} groupsActive={true} eventsActive={false} />
