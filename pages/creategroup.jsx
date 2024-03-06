9831cee7740ef20693454748de8f713bcc987d2e
vasco2024 <156312623+vasco2024@users.noreply.github.com>
2024-03-06 17:31:19 +0000

    xssxsxs

--- pages/creategroup.jsx
+++ pages/creategroup.jsx
@@ -198,7 +198,7 @@ export default function CreateGroup() {
       )}
       <div>
         <button
-          onClick={() => router.push("/events")}
+          onClick={() => router.push("/groups")}
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
         >
           Go Back
