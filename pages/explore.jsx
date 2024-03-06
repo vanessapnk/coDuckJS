ec8a1ed736af3c97e2629fbe3a19573a26423f27
Vanessa Silva <156242127+vanessapnk@users.noreply.github.com>
2024-03-06 17:32:26 +0000

    explore

--- pages/explore.jsx
+++ pages/explore.jsx
@@ -1,15 +1,25 @@
-import { GroupItem } from "@/components/custom/groupItem";
+
 import { Navbar } from "@/components/custom/navbar";
 import { useAuth } from "./context/authContext";
 import { useState } from "react";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+
+
 
 export default function Explore() {
-    const { authenticatedUser } = useAuth();
-    const [userData, setUserData] = useState(null);
+    
     return (
 
         <>
-            <GroupItem />
+            <h1>Explore</h1>
+            < Tabs defaultValue="account" className="w-[full]" >
+                <TabsList>
+                    <TabsTrigger value="groups">Groups</TabsTrigger>
+                    <TabsTrigger value="events">Events</TabsTrigger>
+                </TabsList>
+                <TabsContent value="groups">Make changes to your groups here.</TabsContent>
+                <TabsContent value="events">Change your events here.</TabsContent>
+            </Tabs >
             <Navbar homeActive={false} groupsActive={false} chatActive={true} eventsActive={false} />
         </>
     )
