072a202f0e5dc6c6891ee86c549683ebaa22c719
vasco2024 <156312623+vasco2024@users.noreply.github.com>
2024-03-06 21:46:34 +0000

    a

--- components/event/EventDetails.jsx
+++ components/event/EventDetails.jsx
@@ -29,10 +29,56 @@ export default function EventDetails() {
   }
 
   return (
-    <div>
+    <div className="bg-gray-100 min-h-screen">
       <NavEditEvent backLink={`/events/`} editLink={`/event/edit/${eventId}`} />
-      <h1>{event.name}</h1>
-      <p>{event.description}</p>
+      <div className="bg-white rounded-lg shadow-md mx-auto max-w-md mt-8 p-8">
+        <div className="relative mb-6">
+          <div className="absolute inset-0 bg-gray-300 rounded-lg"></div>
+          <img
+            src="https://via.placeholder.com/800x400"
+            alt="Event Cover"
+            className="w-full h-64 object-cover rounded-lg"
+          />
+        </div>
+        <h1 className="text-3xl font-semibold mb-4">{event.name}</h1>
+        <p className="text-gray-600 mb-4">{event.description}</p>
+        <div className="flex flex-wrap items-center mb-4">
+          <div className="mr-4">
+            <span className="font-semibold">Category:</span> {event.category}
+          </div>
+          <div className="mr-4">
+            <span className="font-semibold">Stack Level:</span>{" "}
+            {event.stackLevel}
+          </div>
+          <div className="mr-4">
+            <span className="font-semibold">Modality:</span> {event.modality}
+          </div>
+          <div className="mr-4">
+            <span className="font-semibold">City:</span> {event.city}
+          </div>
+          <div className="mr-4">
+            <span className="font-semibold">Date:</span>{" "}
+            {new Date(event.date).toLocaleDateString()}
+          </div>
+        </div>
+        <div className="flex items-center">
+          <span className="font-semibold mr-2">Languages Spoken:</span>
+          <ul className="flex">
+            {event.languagesSpoken.map((language, index) => (
+              <li key={index} className="mr-2">
+                {language}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="mt-4">
+          <span className="font-semibold">Users Limit:</span> {event.usersLimit}
+        </div>
+        <div className="mt-4">
+          <span className="font-semibold">Exact Location:</span>{" "}
+          {event.exactLocation}
+        </div>
+      </div>
       <Navbar />
     </div>
   );
--- components/group/GroupDetails.jsx
+++ components/group/GroupDetails.jsx
@@ -1,12 +1,10 @@
-// GroupDetails.js
-
 import React, { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import Link from "next/link";
 import { Navbar } from "../custom/navbar";
 import { NavEditGroup } from "./NavEditGroup";
 
-export default function GroupDetails(userData) {
+export default function GroupDetails() {
   const router = useRouter();
   const { groupId } = router.query; // Get the group ID from the URL query params
   const [group, setGroup] = useState(null);
@@ -15,11 +13,11 @@ export default function GroupDetails(userData) {
   useEffect(() => {
     const fetchGroupData = async () => {
       try {
-        const res = await fetch(`/api/groups/${groupId}`); // Use the dynamic event ID from the URL
+        const res = await fetch(`/api/groups/${groupId}`); // Use the dynamic group ID from the URL
         const data = await res.json();
         setGroup(data);
       } catch (error) {
-        console.error("Error fetching event details:", error);
+        console.error("Error fetching group details:", error);
       }
     };
 
@@ -37,22 +35,53 @@ export default function GroupDetails(userData) {
   }
 
   return (
-    <div>
+    <div className="bg-gray-100 min-h-screen">
       <NavEditGroup
         backLink={`/groups/`}
         editLink={`/group/edit/${groupId}`}
         onNavBarHide={handleNavBarHide}
       />
       {navBarVisible && <Navbar />} {/* Render NavBar only if it's visible */}
-      <h1>{group.name}</h1>
-      <p>{group.description}</p>
-      <p>{group.modality}</p>
-      <p>{group.category}</p>
-      <p>{group.usersLimit}</p>
-      <p>{group.members.length}</p>
-      <Link href={`/chat/${groupId}`}>
-        <button>Group Chat</button>
-      </Link>
+      <div className="bg-white rounded-lg shadow-md mx-auto max-w-md mt-8 p-8">
+        <div className="relative mb-6">
+          <div className="absolute inset-0 bg-gray-300 rounded-lg"></div>
+          <img
+            src="https://via.placeholder.com/800x400"
+            alt="Event Cover"
+            className="w-full h-64 object-cover rounded-lg"
+          />
+        </div>
+        <h1 className="text-3xl font-semibold mb-4">{group.name}</h1>
+        <p className="text-gray-600 mb-4">{group.description}</p>
+        <div className="flex flex-wrap items-center mb-4">
+          <div className="mr-4">
+            <span className="font-semibold">Category:</span> {group.category}
+          </div>
+          <div className="mr-4">
+            <span className="font-semibold">Stack Level:</span>{" "}
+            {group.stackLevel}
+          </div>
+          <div className="mr-4">
+            <span className="font-semibold">Modality:</span> {group.modality}
+          </div>
+          <div className="mr-4">
+            <span className="font-semibold">City:</span> {group.city}
+          </div>
+        </div>
+        <div className="mt-4">
+          <span className="font-semibold">Users Limit:</span> {group.usersLimit}
+        </div>
+        <div className="mt-4">
+          <span className="font-semibold">Members:</span> {group.members.length}
+        </div>
+      </div>
+      <div className="mx-auto max-w-md mt-4">
+        <Link href={`/chat/${groupId}`}>
+          <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded items-center justify-center">
+            Group Chat
+          </button>
+        </Link>
+      </div>
     </div>
   );
 }
