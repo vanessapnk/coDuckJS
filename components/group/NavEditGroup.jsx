570dec8565311b4d5c3bf43295e8507cadf4e756
vasco2024 <156312623+vasco2024@users.noreply.github.com>
2024-03-06 13:20:25 +0000

    edit group

---
+++ components/group/NavEditGroup.jsx
@@ -0,0 +1,153 @@
+// NavEditGroup.js
+
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import { ArrowLeft2, Edit } from "iconsax-react";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+import { Button } from "../ui/button";
+import { UserEdit } from "../profile/userEdit";
+import { DarkModeSwitch } from "../custom/darkModeSwitch";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
+
+const category = ["Frontend", "Backend", "Design", "Data Analysis", "Other"];
+const levels = ["Beginner", "Junior", "Intermediate", "Senior"];
+const modality = ["presential", "hybrid", "online"];
+
+export function NavEditGroup({ backLink, editLink, onNavBarHide }) {
+  const router = useRouter();
+  const { groupId } = router.query; // Get the group ID from the URL query params
+  const [formData, setFormData] = useState({
+    name: "",
+    description: "",
+    category: [],
+    modality: "",
+    stackLevel: "",
+    usersLimit: 0,
+  });
+
+  useEffect(() => {
+    // Fetch data when component mounts
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    return () => {
+      onNavBarHide(); // Call onNavBarHide when component unmounts
+    };
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const res = await fetch(`/api/groups/${groupId}`);
+      const data = await res.json();
+      setFormData(data);
+    } catch (error) {
+      console.error("Error fetching group data:", error);
+    }
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleCheckboxChange = (e) => {
+    const { value, checked } = e.target;
+    const updatedCategories = checked
+      ? [...formData.category, value]
+      : formData.category.filter((cat) => cat !== value);
+    setFormData({
+      ...formData,
+      category: updatedCategories,
+    });
+  };
+
+  const handleSubmit = async () => {
+    try {
+      const res = await fetch(`/api/groups/${groupId}/edit`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      console.log(data);
+      // Refresh the data after update
+      fetchData();
+    } catch (error) {
+      console.error("Error editing group:", error);
+    }
+  };
+
+  return (
+    <>
+      <Drawer>
+        <div className="px-4 py-6 flex flex-row gap-2 justify-between items-center border-b-2 dark:border-zinc-900 h-10">
+          <Link href={backLink}>
+            <div className="cursor-pointer">
+              <ArrowLeft2
+                size="18"
+                className="dark:text-zinc-200"
+                variant="Linear"
+              />
+            </div>
+          </Link>
+          <DarkModeSwitch />
+          <DrawerTrigger>
+            <div className="cursor-pointer">
+              <Edit
+                className="dark:text-zinc-200 "
+                size="18"
+                variant="Linear"
+              />
+            </div>
+          </DrawerTrigger>
+        </div>
+
+        <DrawerContent>
+          <DrawerHeader>
+            <DrawerTitle>Edit Your Group</DrawerTitle>
+            <DrawerDescription>
+              <div className="flex flex-col gap-4">
+                <div className="flex flex-col items-start gap-2">
+                  <Label htmlFor="text">New Group Name</Label>
+                  <input
+                    name="name"
+                    value={formData.name}
+                    onChange={handleChange}
+                    className="py-6 h-10 w-full rounded-md border border-zinc-200 bg-white px-3 py-2 text-sm ring-offset-white file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-zinc-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-zinc-950 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-zinc-800 dark:bg-zinc-950 dark:ring-offset-zinc-950 dark:placeholder:text-zinc-400 dark:focus-visible:ring-zinc-300"
+                    type="text"
+                    placeholder="Write your new name"
+                  />
+                </div>
+              </div>
+            </DrawerDescription>
+          </DrawerHeader>
+          <DrawerFooter>
+            <DrawerClose className="flex items-center gap-2 content-center justify-center">
+              <Button className="w-full" onClick={handleSubmit}>
+                Update
+              </Button>
+              <Button variant="outline">Cancel</Button>
+            </DrawerClose>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    </>
+  );
+}
--- components/profile/userEdit.jsx
+++ components/profile/userEdit.jsx
@@ -1,36 +1,44 @@
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import { Textarea } from "@/components/ui/textarea"
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
 
 export function UserEdit() {
-    return (
-        <>
-            <div className="flex flex-col gap-4">
-                <div className="flex flex-col items-start gap-2">
-                    <Label htmlFor="text">Name</Label>
-                    <Input className="py-6" type="text" placeholder="John Due" />
-                </div>
+  return (
+    <>
+      <div className="flex flex-col gap-4">
+        <div className="flex flex-col items-start gap-2">
+          <Label htmlFor="text">Name</Label>
+          <Input
+            className="py-6"
+            type="text"
+            placeholder="Write your new name"
+          />
+        </div>
 
-                <div className="flex flex-col items-start gap-2">
-                    <Label htmlFor="githubid">GitHub ID</Label>
-                    <Input className="py-6" type="text" placeholder="/githubname" />
-                </div>
+        <div className="flex flex-col items-start gap-2">
+          <Label htmlFor="githubid">GitHub ID</Label>
+          <Input className="py-6" type="text" placeholder="/githubname" />
+        </div>
 
-                <div className="flex flex-col items-start gap-2">
-                    <Label htmlFor="occupation">Occupation</Label>
-                    <Input className="py-6" type="text" placeholder="Pegar do banco de dados o dado atual" />
-                </div>
+        <div className="flex flex-col items-start gap-2">
+          <Label htmlFor="occupation">Occupation</Label>
+          <Input
+            className="py-6"
+            type="text"
+            placeholder="Pegar do banco de dados o dado atual"
+          />
+        </div>
 
-                <div className="flex flex-col items-start gap-2">
-                    <Label htmlFor="location">Location</Label>
-                    <Input className="py-6" type="text" placeholder="Porto-PT" />
-                </div>
+        <div className="flex flex-col items-start gap-2">
+          <Label htmlFor="location">Location</Label>
+          <Input className="py-6" type="text" placeholder="Porto-PT" />
+        </div>
 
-                <div className="flex flex-col items-start gap-2">
-                    <Label htmlFor="about">Location</Label>
-                    <Textarea placeholder="about" />
-                </div>
-            </div>
-        </>
-    )
+        <div className="flex flex-col items-start gap-2">
+          <Label htmlFor="about">Location</Label>
+          <Textarea placeholder="about" />
+        </div>
+      </div>
+    </>
+  );
 }
