a38ace233c61195b2ea421376a3845d79e165e22
vasco2024 <156312623+vasco2024@users.noreply.github.com>
2024-03-06 19:08:28 +0000

    poland

--- pages/createevent.jsx
+++ pages/createevent.jsx
@@ -64,152 +64,187 @@ export default function CreateEvent() {
   };
 
   return (
-    <div>
-      <h1>Create Event</h1>
-      <form
-        onSubmit={handleSubmit}
-        style={{ display: "flex", flexDirection: "column", gap: "10px" }}
-      >
-        <label>
-          Event Name:
+    <div className="p-4">
+      <h1 className="text-2xl font-bold mb-4">Create Event</h1>
+      <form onSubmit={handleSubmit} className="space-y-4">
+        <label className="flex flex-col space-y-2">
+          <span className="text-sm font-semibold">Event Name:</span>
           <input
             type="text"
             name="name"
             value={eventData.name}
             onChange={handleChange}
+            className="border rounded p-2"
           />
         </label>
-        <label>
-          Creator:
+
+        {/*         <label className="flex flex-col space-y-2">
+          <span className="text-sm font-semibold">Creator:</span>
           <input
             type="text"
             name="creator"
             value={eventData.creator}
             onChange={handleChange}
+            className="border rounded p-2"
           />
-        </label>
-        <label>
-          Description:
+        </label> */}
+
+        <label className="flex flex-col space-y-2">
+          <span className="text-sm font-semibold">Description:</span>
           <input
             type="text"
             name="description"
             value={eventData.description}
             onChange={handleChange}
+            className="border rounded p-2"
           />
         </label>
-        <label>
-          Category:
-          <input
-            type="text"
-            name="category"
-            value={eventData.category}
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          Modality:
+
+        <label className="text-sm font-semibold mr-3">Category</label>
+        <select
+          name="category"
+          value={eventData.category}
+          onChange={handleChange}
+          className="border rounded p-2"
+        >
+          <option value="">Select Category</option>
+          <option value="Frontend">Frontend</option>
+          <option value="Backend">Backend</option>
+          <option value="Design">Design</option>
+          <option value="Data Analysis">Data Analysis</option>
+          <option value="Other">Other</option>
+        </select>
+
+        <label className="text-sm font-semibold mr-3">Modality</label>
+        <select
+          name="modality"
+          value={eventData.modality}
+          onChange={handleChange}
+          className="border rounded p-2"
+        >
+          <option value="">Select Modality</option>
+          <option value="presential">Presential</option>
+          <option value="hybrid">Hybrid</option>
+          <option value="online">Online</option>
+          <option value="Other">Other</option>
+        </select>
+
+        {/* <label className="flex flex-col space-y-2">
+          <span className="text-sm font-semibold">Modality:</span>
           <input
             type="text"
             name="modality"
             value={eventData.modality}
             onChange={handleChange}
+            className="border rounded p-2"
           />
-        </label>
-        <label>
-          City:
+        </label> */}
+
+        <label className="flex flex-col space-y-2">
+          <span className="text-sm font-semibold">City:</span>
           <input
             type="text"
             name="city"
             value={eventData.city}
             onChange={handleChange}
+            className="border rounded p-2"
           />
         </label>
-        <label>
-          Users Limit:
+        <label className="flex flex-col space-y-2">
+          <span className="text-sm font-semibold">Users Limit:</span>
           <input
             type="number"
             name="usersLimit"
             value={eventData.usersLimit}
             onChange={handleChange}
+            className="border rounded p-2"
           />
         </label>
-        <label>
-          Exact Location:
+        <label className="flex flex-col space-y-2">
+          <span className="text-sm font-semibold">Exact Location:</span>
           <input
             type="text"
             name="exactLocation"
             value={eventData.exactLocation}
             onChange={handleChange}
+            className="border rounded p-2"
           />
         </label>
-        <label>
-          Date:
-          <input
-            type="date"
-            name="date"
-            value={eventData.date}
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          End Date:
-          <input
-            type="date"
-            name="endDate"
-            value={eventData.endDate}
-            onChange={handleChange}
-          />
-        </label>
-        {/* Add label and checkboxes for stacks */}
-        <label>
-          Stacks:
-          {stacksList.map((stack) => (
-            <div key={stack}>
-              <input
-                type="checkbox"
-                name="stacks"
-                value={stack}
-                checked={eventData.stacks.includes(stack)}
-                onChange={handleChange}
-              />
-              {stack}
-            </div>
-          ))}
-        </label>
-        {/* Add label and checkboxes for languagesSpoken */}
-        <label>
-          Languages Spoken:
-          {languageList.map((language) => (
-            <div key={language}>
-              <input
-                type="checkbox"
-                name="languagesSpoken"
-                value={language}
-                checked={eventData.languagesSpoken.includes(language)}
-                onChange={handleChange}
-              />
-              {language}
-            </div>
-          ))}
-        </label>
-        <button type="submit">Create Event</button>
+        <div className="flex flex-row space-x-4">
+          <label className="flex flex-col space-y-2">
+            <span className="text-sm font-semibold">Date:</span>
+            <input
+              type="date"
+              name="date"
+              value={eventData.date}
+              onChange={handleChange}
+              className="border rounded p-2 w-32" // Adjust the width here
+            />
+          </label>
+          <label className="flex flex-col space-y-2">
+            <span className="text-sm font-semibold">End Date:</span>
+            <input
+              type="date"
+              name="endDate"
+              value={eventData.endDate}
+              onChange={handleChange}
+              className="border rounded p-2 w-32" // Adjust the width here
+            />
+          </label>
+        </div>
+
+        <div className="flex flex-col space-y-2">
+          <span className="text-sm font-semibold">Stacks:</span>
+          <div className="grid grid-cols-3 gap-2">
+            {stacksList.map((stack) => (
+              <div key={stack} className="flex items-center">
+                <input
+                  type="checkbox"
+                  name="stacks"
+                  value={stack}
+                  checked={eventData.stacks.includes(stack)}
+                  onChange={handleChange}
+                  className="mr-1"
+                />
+                <span>{stack}</span>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <div className="flex flex-col space-y-2">
+          <span className="text-sm font-semibold">Languages Spoken:</span>
+          <div className="grid grid-cols-3 gap-2">
+            {languageList.map((language) => (
+              <div key={language} className="flex items-center">
+                <input
+                  type="checkbox"
+                  name="languagesSpoken"
+                  value={language}
+                  checked={eventData.languagesSpoken.includes(language)}
+                  onChange={handleChange}
+                  className="mr-1"
+                />
+                <span>{language}</span>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <button
+          type="submit"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+          Create Event
+        </button>
       </form>
       {eventCreated && (
-        <div>
-          <p>Name: {eventData.name}</p>
-          <p>Creator: {eventData.creator}</p>
-          <p>Description: {eventData.description}</p>
-          <p>Category: {eventData.category}</p>
-          <p>Modality: {eventData.modality}</p>
-          <p>City: {eventData.city}</p>
-          <p>Users Limit: {eventData.usersLimit}</p>
-          <p>Exact Location: {eventData.exactLocation}</p>
-          <p>Date: {eventData.date}</p>
-          <p>End Date: {eventData.endDate}</p>
-          <p>Event created successfully!</p>
+        <div className="mt-4">
+          <p className="text-lg font-semibold">Event created successfully!</p>
+          {/* Display event data */}
         </div>
       )}
-      <div>
+      <div className="mt-4">
         <button
           onClick={() => router.push("/events")}
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
@@ -222,7 +257,7 @@ export default function CreateEvent() {
 }
 
 const stacksList = [
-  "HTML & CSS",
+  "HTML",
   "CSS",
   "JavaScript",
   "TypeScript",
@@ -230,7 +265,7 @@ const stacksList = [
   "React",
   "NextJs",
   "MongoDB",
-  "UI/UX Design",
+  "Design",
 ];
 const languageList = [
   "English",
