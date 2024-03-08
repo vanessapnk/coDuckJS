93ed37ed687574d445be7dafa04922fd9f0efd30
vasco2024 <156312623+vasco2024@users.noreply.github.com>
2024-03-08 11:30:05 +0000

    ss

--- components/profile/userTop.jsx
+++ components/profile/userTop.jsx
@@ -57,7 +57,8 @@ export function UserTop({
       <div className="flex flex-col gap-1">
         <h1 className="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
           <span className="text-transparent bg-clip-text bg-gradient-to-r to-custom-green from-custom-yellow">
-            User Bio
+            {/* User Bio */}
+            {name}
           </span>
           {/*  Scalable AI. */}
           <p className="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">
